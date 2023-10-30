92ad8df39517ff7c9e637b56a8794aa03d58a6b8
FFBROWN <40918320+FFBROWN@users.noreply.github.com>
2023-10-30 10:11:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,13 @@
-const questions = [
-    
-  
-{
-        question: "Which subatomic particle has a positive charge?",
-        answers:[
-        {text: "Proton", correct:true},
-        {text: "Electron", correct:false},
-        {text: "Neutron", correct:false},
-        {text: "Quark", correct:false},
-        ]
+// ... (previous code)
 
-    },
-    {
-        question: "What is found in the nucleus of an atom?",
-        answers:[
-        {text: "Electrons", correct:false},
-        {text: "Neutron", correct:false},
-        {text: "Protons", correct:false},
-        {text: "Both B and C", correct:true},
-        ]
-
-    },
-    {
-        question: "Which element has the atomic number 6?",
-        answers:[
-        {text: "Oxygen", correct:false},
-        {text: "Hydrogen", correct:false},
-        {text: "Carbon", correct:true},
-        {text: "Helium", correct:false},
-        ]
-
-    },
-    {
-        question: "What is the charge of an electron?",
-        answers:[
-        {text: "Postive", correct:false},
-        {text: "Negative", correct:true},
-        {text: "neutral", correct:false},
-        {text: "Variable", correct:false},
-        ]
-
-    },
-   
-];
-
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-
-let currentQuestionIndex = 0;
-let score = 0;
-
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
-
 }
-function showQuestion(){
+
+function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
@@ -68,65 +17,62 @@ function showQuestion(){
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
-        if(answer.correct){
+        answerButtons.appendChild(button);
+        if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
 
         button.addEventListener("click", selectAnswer);
     });
-
 }
 
-function resetState(){
-    nextButton.style.display = none;
-    while(answerButtons.firstChild){
+function resetState() {
+    nextButton.style.display = 'none';
+    while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
-function selectAnswer(){
+function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
-    if (isCorrect){
+    if (isCorrect) {
         selectedBtn.classList.add("correct");
         score++;
-
-    }else{
+    } else {
         selectedBtn.classList.add("incorrect");
-}
-    Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true"){
-        button.classList.add("correct");
     }
-    button.disabled = true;
-});
-nextButton.style.display = "block";
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    }
+    nextButton.style.display = "block";
 }
-function showScore(){
+
+function showScore() {
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
 }
 
-
-function handleNextButton(){
+function handleNextButton() {
     currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
+    if (currentQuestionIndex < questions.length) {
         showQuestion();
-    }else{
+    } else {
         showScore();
     }
 }
 
-nextButton.addEventListener("click", ()=>{
-    if(currentQuestionIndex < questions.length){
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
         handleNextButton();
     } else {
-        startQuiz;
+        startQuiz();
     }
 });
-startQuiz();
 
-  
+startQuiz();
